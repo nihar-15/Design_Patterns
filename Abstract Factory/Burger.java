cad3b9518fdfb73497defdb2a0075a461d78ffb5
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-03-28 20:00:29 +0530

    Add files via upload

---
+++ Abstract Factory/Burger.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.abstractfactorydemo;
+
+/**
+ *
+ * @author ACER
+ */
+public class Burger extends Item{
+    
+    
+    Burger(){
+        item ="Burger";
+    }
+    
+    @Override
+    public Item getItem(){
+        return this;
+    }
+}
---
+++ Abstract Factory/Container.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.abstractfactorydemo;
+
+/**
+ *
+ * @author ACER
+ */
+public interface Container {
+    
+    
+   public String getContainer();
+}
